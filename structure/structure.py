20e893d6aca03a78b8e30e65c22e8d64945e886b
Victor Curean <victor.curean@unicle.life>
2023-02-19 01:58:04 +0100

    initial commit

---
+++ structure/structure.py
@@ -0,0 +1,79 @@
+import Bio.PDB.Selection
+from utils import *
+from Bio.PDB import NeighborSearch, PDBParser
+
+parser = PDBParser(PERMISSIVE=False)
+
+def get_structure(pdb_id, file):
+    return parser.get_structure(id=pdb_id, file=file)
+
+def get_opposite_complex(origin, targets):
+    type_origin = origin.get_full_id()[2]
+    targets_to_keep = list()
+    for t in targets:
+        #origin is antigen, target in heavy- or light chain:
+        if t.get_full_id()[2] in ["H", "L"] and type_origin == "C":
+            targets_to_keep.append(t)
+        #origin in heavy- or light chain, target in antigen
+        elif t.get_full_id()[2] == "C" and type_origin in ["H", "L"]:
+            targets_to_keep.append(t)
+
+    return set(targets_to_keep)
+
+def get_neighboring_residues(struct, target_residue, cutoff):
+    atoms = list(struct.get_atoms())
+    pdb_nbsearch = Bio.PDB.NeighborSearch(atoms)
+    res_atoms = target_residue.get_atoms()
+
+    neighboring_residues = set()
+    for a in list(res_atoms):
+        neighboring_residues |= set(pdb_nbsearch.search(a.get_coord(), cutoff, level='R'))
+
+    return get_opposite_complex(target_residue, neighboring_residues)
+
+def get_residues_by_type(struct, type):
+    # H - heavy chain, L - light chain, C - antigen
+    return [r for r in struct.get_residues() if r.get_full_id()[2] == type]
+
+def write_annotated_sequence(file, struct, type):
+    SEQ = ""
+    ANN = ""
+    for r in get_residues_by_type(struct, type):
+        SEQ += AA_3to1[r.get_resname()]
+        nb_res = get_neighboring_residues(struct, r, cutoff=5)
+        if len(nb_res) != 0:
+            ANN += "#"
+        else:
+            ANN += "_"
+    file.write(SEQ + "\n")
+    file.write(ANN + "\n")
+
+def annotate_sequence_by_region(seq):
+    res = number_a_sequence(seq)
+
+def annotate_sequence(struct, pdb_id):
+    OUT_DIR = "C:\\Users\\curea\\PycharmProjects\\kul-thesis-ab\\annotated_sequences\\"
+    with open(OUT_DIR+pdb_id+".seqann", "w") as file:
+        file.write("ID\t" + pdb_id + "\n")
+
+        #LIGHT CHAIN
+        file.write("Light chain" + "\n")
+        write_annotated_sequence(file, struct, "L")
+
+        #HEAVY CHAIN
+        file.write("Heavy chain" + "\n")
+        write_annotated_sequence(file, struct, "H")
+
+        #ANTIGEN
+        file.write("Antigen" + "\n")
+        write_annotated_sequence(file, struct, "C")
+
+struct = get_structure("1A2Y", "C:\\Users\\curea\\PycharmProjects\\kul-thesis-ab\\datasets\\1A2Y_1.pdb")
+
+for r in struct.get_residues():
+    a = get_neighboring_residues(struct, r, cutoff=5)
+    if len(a) != 0:
+        b = 1
+
+
+annotate_sequence(struct, "1A2Y")
---
+++ structure/utils.py
@@ -0,0 +1,24 @@
+import requests
+
+
+AA_3to1 = {'CYS': 'C', 'ASP': 'D', 'SER': 'S', 'GLN': 'Q', 'LYS': 'K',
+     'ILE': 'I', 'PRO': 'P', 'THR': 'T', 'PHE': 'F', 'ASN': 'N',
+     'GLY': 'G', 'HIS': 'H', 'LEU': 'L', 'ARG': 'R', 'TRP': 'W',
+     'ALA': 'A', 'VAL':'V', 'GLU': 'E', 'TYR': 'Y', 'MET': 'M'}
+
+
+MARTIN_NUMBERING = {"LFR1": (1, 23), "CDR-L1": (24, 34), "LFR2": (35, 49), "CDR-L2": (50, 56), "LFR3": (57, 88),
+                    "CDR-L3": (89, 97), "LFR4": (98, 110), "HFR1": (1, 30), "CDR-H1": (31, 35), "HFR2": (36, 49),
+                    "CDR-H2": (50, 65), "HFR3": (66, 94), "CDR-H3": (95, 102), "HFR4": (103, 113)}
+
+MARTIN_NUMBERING_LFR = list(range(1, 24)) + list(range(35, 50)) + list(range(57, 89)) + list(range(98, 111))
+MARTIN_NUMBERING_HFR = list(range(1, 31)) + list(range(36, 50)) + list(range(66, 95)) + list(range(103, 113))
+
+
+def number_a_sequence(seq):
+    API_NUMERBING_URL = "http://www.bioinf.org.uk/abs/abnum/abnum.cgi?plain=1&aaseq={seq}&scheme={scheme}"
+
+    response = requests.get(API_NUMERBING_URL.format(seq=seq, scheme="-m"))
+    return response.text
+
+number_a_sequence("QVQLQESGPGLVAPSQSLSITCTVSGFSLTGYGVNWVRQPPGKGLEWLGMIWGDGNTDYNSALKSRLSISKDNSKSQVFLKMNSLHTDDTARYYCARERDYRLDYWGQGTTLTVSS")
