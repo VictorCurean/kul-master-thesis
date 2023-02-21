9b89f98cebbcf0a00d59b68ad2eda69164afb8db
Victor Curean <victor.curean@unicle.life>
2023-02-21 16:00:56 +0100

    motif finding

---
+++ motifs/testing.py
@@ -0,0 +1,11 @@
+from structure import get_structure, get_neighboring_residues, annotate_sequence
+
+
+struct = get_structure("1A14", "C:\\Users\\curea\\Documents\\Thesis Code\\kul-master-thesis\\kul-master-thesis\\kul-thesis-ab\\datasets\\1A14_1.pdb")
+for r in struct.get_residues():
+    a = get_neighboring_residues(struct, r, cutoff=5)
+    if len(a) != 0:
+        b = 1
+
+
+annotate_sequence(struct, "1A14")
